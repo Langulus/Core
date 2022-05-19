0b385e4c4b1eddf411e861a08ced632b51f54c62
Epixu <dimomarkov@gmail.com>
2022-05-19 15:12:55 +0300

    Initial commit

---
+++ include/Langulus.Core.hpp
@@ -0,0 +1,37 @@
+///																									
+/// Langulus::Core																			   
+/// Copyright(C) 2012 Dimo Markov <langulusteam@gmail.com>					      
+///																									
+/// Distributed under GNU General Public License v3+									
+/// See LICENSE file, or https://www.gnu.org/licenses									
+///																									
+#pragma once
+#include "../Abs.hpp"
+#include "../Add.hpp"
+#include "../Ceil.hpp"
+#include "../Ceil.hpp"
+#include "../Convert.hpp"
+#include "../Divide.hpp"
+#include "../Equals.hpp"
+#include "../EqualsOrGreater.hpp"
+#include "../EqualsOrLower.hpp"
+#include "../Fill.hpp"
+#include "../Floor.hpp"
+#include "../Greater.hpp"
+#include "../Intrinsics.hpp"
+#include "../Lesser.hpp"
+#include "../Load.hpp"
+#include "../Log.hpp"
+#include "../Max.hpp"
+#include "../Min.hpp"
+#include "../MoreSIMD.hpp"
+#include "../Multiply.hpp"
+#include "../Pow.hpp"
+#include "../Round.hpp"
+#include "../SetGet.hpp"
+#include "../ShiftLeft.hpp"
+#include "../ShiftRight.hpp"
+#include "../SIMD.hpp"
+#include "../Store.hpp"
+#include "../Subtract.hpp"
+#include "../XOr.hpp"
