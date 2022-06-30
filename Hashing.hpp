f8722d4657ca00166ae16f11d566cd0d4849fcd5
Epixu <dimomarkov@gmail.com>
2022-06-30 14:31:20 +0300

    Added murmur hashing

--- Hashing.hpp
+++ Hashing.hpp
@@ -8,6 +8,12 @@
 #pragma once
 #include "Common.hpp"
 
+#if defined(_MSC_VER)
+	#define BIG_CONSTANT(x) (x)
+#else
+	#define BIG_CONSTANT(x) (x##LLU)
+#endif
+
 namespace Langulus
 {
 	namespace Inner
@@ -15,7 +21,9 @@ namespace Langulus
 
 		/// MurmurHash3 was written by Austin Appleby, and is placed in the		
 		/// public domain																			
+		/// https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
 		void MurmurHash3_x86_32(const void* key, int len, uint32_t seed, void* out);
+		void MurmurHash2_x64_64(const void* key, int len, uint64_t seed, void* out);
 		void MurmurHash3_x86_128(const void* key, int len, uint32_t seed, void* out);
 		void MurmurHash3_x64_128(const void* key, int len, uint32_t seed, void* out);
 
@@ -23,81 +31,459 @@ namespace Langulus
 		/// as the algorithms are optimized for their respective platforms.		
 		/// You can still compile and run any of them on any platform, but your	
 		/// performance with the non-native version will be less than optimal.	
-		#if defined(_MSC_VER)
-			#define ROTL32(x,y)	_rotl(x,y)
-			#define ROTL64(x,y)	_rotl64(x,y)
-			#define BIG_CONSTANT(x) (x)
-		#else
-			inline uint32_t rotl32(uint32_t x, int8_t r) {
-				return (x << r) | (x >> (32 - r));
+
+		/// Block read - if your platform needs to do endian-swapping or can		
+		/// only handle aligned reads, do the conversion here							
+		LANGULUS(ALWAYSINLINE) uint32_t getblock32(const uint32_t* p, int i) {
+			return p[i];
+		}
+
+		LANGULUS(ALWAYSINLINE) uint64_t getblock64(const uint64_t* p, int i) {
+			return p[i];
+		}
+
+		/// Finalization mix - force all bits of a hash block to avalanche		
+		LANGULUS(ALWAYSINLINE) uint32_t fmix32(uint32_t h) {
+			h ^= h >> 16;
+			h *= 0x85ebca6b;
+			h ^= h >> 13;
+			h *= 0xc2b2ae35;
+			h ^= h >> 16;
+			return h;
+		}
+
+		LANGULUS(ALWAYSINLINE) uint64_t fmix64(uint64_t k) {
+			k ^= k >> 33;
+			k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+			k ^= k >> 33;
+			k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+			k ^= k >> 33;
+			return k;
+		}
+
+		/// 32-bit hasher optimized for x86													
+		///	@attention key memory must be aligned to 4 bytes						
+		///	@tparam TAIL - set to true, if length is not aligned to 4 bytes	
+		///	@param key - the memory to hash												
+		///	@param len - the number of bytes in the key								
+		///	@param seed - the seed for the hash											
+		///	@param out - [out] the hash goes here (must be 4 bytes)				
+		template<bool TAIL = true>
+		void MurmurHash3_x86_32(const void* key, int len, uint32_t seed, void* out) {
+			const uint8_t* data = (const uint8_t*) key;
+			const int nblocks = len / 4;
+			uint32_t h1 = seed;
+			const uint32_t c1 = 0xcc9e2d51;
+			const uint32_t c2 = 0x1b873593;
+			
+			// Body																			
+			const uint32_t* blocks = (const uint32_t*) (data + nblocks * 4);
+			for (int i = -nblocks; i; i++) {
+				uint32_t k1 = getblock32(blocks, i);
+
+				k1 *= c1;
+				k1 = ::std::rotl(k1, 15);
+				k1 *= c2;
+
+				h1 ^= k1;
+				h1 = ::std::rotl(h1, 13);
+				h1 = h1 * 5 + 0xe6546b64;
 			}
 
-			inline uint64_t rotl64(uint64_t x, int8_t r) {
-				return (x << r) | (x >> (64 - r));
+			// Tail																			
+			if constexpr (TAIL) {
+				const uint8_t* tail = (const uint8_t*) (data + nblocks * 4);
+				uint32_t k1 = 0;
+				switch (len & 3) {
+				case 3:
+					k1 ^= tail[2] << 16;
+					[[fallthrough]];
+				case 2:
+					k1 ^= tail[1] << 8;
+					[[fallthrough]];
+				case 1:
+					k1 ^= tail[0];
+					k1 *= c1;
+					k1 = ::std::rotl(k1, 15);
+					k1 *= c2;
+					h1 ^= k1;
+				};
 			}
 
-			#define ROTL32(x,y)	rotl32(x,y)
-			#define ROTL64(x,y)	rotl64(x,y)
-			#define BIG_CONSTANT(x) (x##LLU)
-		#endif // !defined(_MSC_VER)
+			// Finalization																
+			h1 ^= len;
+			h1 = fmix32(h1);
+			*(uint32_t*) out = h1;
+		}
+
+		/// 128-bit hasher optimized for x86												
+		///	@attention key memory must be aligned to 16 bytes						
+		///	@tparam TAIL - set to true, if length is not aligned to 16 bytes	
+		///	@param key - the memory to hash												
+		///	@param len - the number of bytes in the key								
+		///	@param seed - the seed for the hash											
+		///	@param out - [out] the hash goes here (must be 16 bytes)				
+		template<bool TAIL = true>
+		void MurmurHash3_x86_128(const void* key, const int len, uint32_t seed, void* out) {
+			const uint8_t* data = (const uint8_t*) key;
+			const int nblocks = len / 16;
+
+			uint32_t h1 = seed;
+			uint32_t h2 = seed;
+			uint32_t h3 = seed;
+			uint32_t h4 = seed;
+
+			const uint32_t c1 = 0x239b961b;
+			const uint32_t c2 = 0xab0e9789;
+			const uint32_t c3 = 0x38b34ae5;
+			const uint32_t c4 = 0xa1e38b93;
+
+			// Body																			
+			const uint32_t* blocks = (const uint32_t*) (data + nblocks * 16);
+			for (int i = -nblocks; i; i++) {
+				uint32_t k1 = getblock32(blocks, i * 4 + 0);
+				uint32_t k2 = getblock32(blocks, i * 4 + 1);
+				uint32_t k3 = getblock32(blocks, i * 4 + 2);
+				uint32_t k4 = getblock32(blocks, i * 4 + 3);
+
+				k1 *= c1; 
+				k1 = ::std::rotl(k1, 15);
+				k1 *= c2; 
+				h1 ^= k1;
+
+				h1 = ::std::rotl(h1, 19);
+				h1 += h2;
+				h1 = h1 * 5 + 0x561ccd1b;
+
+				k2 *= c2;
+				k2 = ::std::rotl(k2, 16);
+				k2 *= c3;
+				h2 ^= k2;
+
+				h2 = ::std::rotl(h2, 17);
+				h2 += h3;
+				h2 = h2 * 5 + 0x0bcaa747;
+
+				k3 *= c3;
+				k3 = ::std::rotl(k3, 17);
+				k3 *= c4;
+				h3 ^= k3;
+
+				h3 = ::std::rotl(h3, 15);
+				h3 += h4;
+				h3 = h3 * 5 + 0x96cd1c35;
+
+				k4 *= c4;
+				k4 = ::std::rotl(k4, 18);
+				k4 *= c1;
+				h4 ^= k4;
+
+				h4 = ::std::rotl(h4, 13);
+				h4 += h1;
+				h4 = h4 * 5 + 0x32ac3b17;
+			}
+
+			// Tail																			
+			if constexpr (TAIL) {
+				const uint8_t* tail = (const uint8_t*) (data + nblocks * 16);
+
+				uint32_t k1 = 0;
+				uint32_t k2 = 0;
+				uint32_t k3 = 0;
+				uint32_t k4 = 0;
+
+				switch (len & 15) {
+				case 15:
+					k4 ^= tail[14] << 16;
+					[[fallthrough]];
+				case 14:
+					k4 ^= tail[13] << 8;
+					[[fallthrough]];
+				case 13:
+					k4 ^= tail[12] << 0;
+					k4 *= c4;
+					k4 = ::std::rotl(k4, 18);
+					k4 *= c1;
+					h4 ^= k4;
+					[[fallthrough]];
+				case 12:
+					k3 ^= tail[11] << 24;
+					[[fallthrough]];
+				case 11:
+					k3 ^= tail[10] << 16;
+					[[fallthrough]];
+				case 10:
+					k3 ^= tail[9] << 8;
+					[[fallthrough]];
+				case  9:
+					k3 ^= tail[8] << 0;
+					k3 *= c3;
+					k3 = ::std::rotl(k3, 17);
+					k3 *= c4;
+					h3 ^= k3;
+					[[fallthrough]];
+				case  8:
+					k2 ^= tail[7] << 24;
+					[[fallthrough]];
+				case  7:
+					k2 ^= tail[6] << 16;
+					[[fallthrough]];
+				case  6:
+					k2 ^= tail[5] << 8;
+					[[fallthrough]];
+				case  5:
+					k2 ^= tail[4] << 0;
+					k2 *= c2;
+					k2 = ::std::rotl(k2, 16);
+					k2 *= c3;
+					h2 ^= k2;
+					[[fallthrough]];
+				case  4:
+					k1 ^= tail[3] << 24;
+					[[fallthrough]];
+				case  3:
+					k1 ^= tail[2] << 16;
+					[[fallthrough]];
+				case  2:
+					k1 ^= tail[1] << 8;
+					[[fallthrough]];
+				case  1:
+					k1 ^= tail[0] << 0;
+					k1 *= c1;
+					k1 = ::std::rotl(k1, 15);
+					k1 *= c2;
+					h1 ^= k1;
+				};
+			}
+
+			// Finalization																
+			h1 ^= len;
+			h2 ^= len;
+			h3 ^= len;
+			h4 ^= len;
+
+			h1 += h2;
+			h1 += h3;
+			h1 += h4;
+			h2 += h1;
+			h3 += h1;
+			h4 += h1;
+
+			h1 = fmix32(h1);
+			h2 = fmix32(h2);
+			h3 = fmix32(h3);
+			h4 = fmix32(h4);
+
+			h1 += h2;
+			h1 += h3;
+			h1 += h4;
+			h2 += h1;
+			h3 += h1;
+			h4 += h1;
+
+			((uint32_t*) out)[0] = h1;
+			((uint32_t*) out)[1] = h2;
+			((uint32_t*) out)[2] = h3;
+			((uint32_t*) out)[3] = h4;
+		}
+
+		/// 64-bit hasher optimized for x86													
+		///	@attention key memory must be aligned to 8 bytes						
+		///	@tparam TAIL - set to true, if length is not aligned to 8 bytes	
+		///	@param key - the memory to hash												
+		///	@param len - the number of bytes in the key								
+		///	@param seed - the seed for the hash											
+		///	@param out - [out] the hash goes here (must be 8 bytes)				
+		template<bool TAIL = true>
+		void MurmurHash2_x64_64(const void* key, int len, uint64_t seed, void* out) {
+			const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+			const int r = 47;
+			uint64_t h = seed ^ (len * m);
+			const uint64_t* data = (const uint64_t*) key;
+			const uint64_t* end = data + (len / 8);
+
+			while (data != end) {
+				uint64_t k = *data++;
+
+				k *= m;
+				k ^= k >> r;
+				k *= m;
+
+				h ^= k;
+				h *= m;
+			}
+
+			if constexpr (TAIL) {
+				const unsigned char* data2 = (const unsigned char*) data;
+				switch (len & 7) {
+				case 7:
+					h ^= uint64_t(data2[6]) << 48;
+					[[fallthrough]];
+				case 6:
+					h ^= uint64_t(data2[5]) << 40;
+					[[fallthrough]];
+				case 5:
+					h ^= uint64_t(data2[4]) << 32;
+					[[fallthrough]];
+				case 4:
+					h ^= uint64_t(data2[3]) << 24;
+					[[fallthrough]];
+				case 3:
+					h ^= uint64_t(data2[2]) << 16;
+					[[fallthrough]];
+				case 2:
+					h ^= uint64_t(data2[1]) << 8;
+					[[fallthrough]];
+				case 1:
+					h ^= uint64_t(data2[0]);
+					h *= m;
+				};
+			}
+
+			h ^= h >> r;
+			h *= m;
+			h ^= h >> r;
+			*(uint64_t*) out = h;
+		}
+
+		/// 128-bit hasher optimized for x64												
+		///	@attention key memory must be aligned to 16 bytes						
+		///	@tparam TAIL - set to true, if length is not aligned to 16 bytes	
+		///	@param key - the memory to hash												
+		///	@param len - the number of bytes in the key								
+		///	@param seed - the seed for the hash											
+		///	@param out - [out] the hash goes here (must be 16 bytes)				
+		template<bool TAIL = true>
+		void MurmurHash3_x64_128(const void* key, const int len, const uint32_t seed, void* out) {
+			const uint8_t* data = (const uint8_t*) key;
+			const int nblocks = len / 16;
+
+			uint64_t h1 = seed;
+			uint64_t h2 = seed;
+
+			const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+			const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+
+			// Body																			
+			const uint64_t* blocks = (const uint64_t*) (data);
+			for (int i = 0; i < nblocks; i++) {
+				uint64_t k1 = getblock64(blocks, i * 2 + 0);
+				uint64_t k2 = getblock64(blocks, i * 2 + 1);
+
+				k1 *= c1;
+				k1 = ::std::rotl(k1, 31);
+				k1 *= c2;
+				h1 ^= k1;
+
+				h1 = ::std::rotl(h1, 27);
+				h1 += h2;
+				h1 = h1 * 5 + 0x52dce729;
+
+				k2 *= c2;
+				k2 = ::std::rotl(k2, 33);
+				k2 *= c1;
+				h2 ^= k2;
+
+				h2 = ::std::rotl(h2, 31);
+				h2 += h1;
+				h2 = h2 * 5 + 0x38495ab5;
+			}
+
+			// Tail																			
+			if constexpr (TAIL) {
+				const uint8_t* tail = (const uint8_t*) (data + nblocks * 16);
+				uint64_t k1 = 0;
+				uint64_t k2 = 0;
+
+				switch (len & 15) {
+				case 15:
+					k2 ^= ((uint64_t) tail[14]) << 48;
+					[[fallthrough]];
+				case 14:
+					k2 ^= ((uint64_t) tail[13]) << 40;
+					[[fallthrough]];
+				case 13:
+					k2 ^= ((uint64_t) tail[12]) << 32;
+					[[fallthrough]];
+				case 12:
+					k2 ^= ((uint64_t) tail[11]) << 24;
+					[[fallthrough]];
+				case 11:
+					k2 ^= ((uint64_t) tail[10]) << 16;
+					[[fallthrough]];
+				case 10:
+					k2 ^= ((uint64_t) tail[9]) << 8;
+					[[fallthrough]];
+				case  9:
+					k2 ^= ((uint64_t) tail[8]) << 0;
+					k2 *= c2;
+					k2 = ROTL64(k2, 33);
+					k2 *= c1;
+					h2 ^= k2;
+					[[fallthrough]];
+				case  8:
+					k1 ^= ((uint64_t) tail[7]) << 56;
+					[[fallthrough]];
+				case  7:
+					k1 ^= ((uint64_t) tail[6]) << 48;
+					[[fallthrough]];
+				case  6:
+					k1 ^= ((uint64_t) tail[5]) << 40;
+					[[fallthrough]];
+				case  5:
+					k1 ^= ((uint64_t) tail[4]) << 32;
+					[[fallthrough]];
+				case  4:
+					k1 ^= ((uint64_t) tail[3]) << 24;
+					[[fallthrough]];
+				case  3:
+					k1 ^= ((uint64_t) tail[2]) << 16;
+					[[fallthrough]];
+				case  2:
+					k1 ^= ((uint64_t) tail[1]) << 8;
+					[[fallthrough]];
+				case  1:
+					k1 ^= ((uint64_t) tail[0]) << 0;
+					k1 *= c1;
+					k1 = ::std::rotl(k1, 31);
+					k1 *= c2;
+					h1 ^= k1;
+				};
+			}
+
+			// Finalization																
+			h1 ^= len;
+			h2 ^= len;
+
+			h1 += h2;
+			h2 += h1;
+
+			h1 = fmix64(h1);
+			h2 = fmix64(h2);
+
+			h1 += h2;
+			h2 += h1;
+
+			((uint64_t*) out)[0] = h1;
+			((uint64_t*) out)[1] = h2;
+		}
 	}
 
 	/// Hash a sequence of bytes																
 	///	@param ptr - memory start															
 	///	@param len - number of bytes to hash											
 	///	@return the hash																		
-	inline Hash HashBytes(void const* ptr, size_t len) noexcept {
-		static constexpr uint64_t m = UINT64_C(0xc6a4a7935bd1e995);
-		static constexpr uint64_t seed = UINT64_C(0xe17a1465);
-		static constexpr unsigned int r = 47;
-
-		auto const* const data64 = static_cast<uint64_t const*>(ptr);
-		uint64_t h = seed ^ (len * m);
-
-		size_t const n_blocks = len / 8;
-		for (size_t i = 0; i < n_blocks; ++i) {
-			auto k = Inner::unaligned_load<uint64_t>(data64 + i);
-
-			k *= m;
-			k ^= k >> r;
-			k *= m;
-
-			h ^= k;
-			h *= m;
-		}
-
-		auto const* const data8 = reinterpret_cast<uint8_t const*>(data64 + n_blocks);
-		switch (len & 7U) {
-		case 7:
-			h ^= static_cast<uint64_t>(data8[6]) << 48U;
-			[[fallthrough]];
-		case 6:
-			h ^= static_cast<uint64_t>(data8[5]) << 40U;
-			[[fallthrough]];
-		case 5:
-			h ^= static_cast<uint64_t>(data8[4]) << 32U;
-			[[fallthrough]];
-		case 4:
-			h ^= static_cast<uint64_t>(data8[3]) << 24U;
-			[[fallthrough]];
-		case 3:
-			h ^= static_cast<uint64_t>(data8[2]) << 16U;
-			[[fallthrough]];
-		case 2:
-			h ^= static_cast<uint64_t>(data8[1]) << 8U;
-			[[fallthrough]];
-		case 1:
-			h ^= static_cast<uint64_t>(data8[0]);
-			h *= m;
-			[[fallthrough]];
-		default:
-			break;
-		}
-
-		h ^= h >> r;
-		//h *= m;
-		//h ^= h >> r;
-		return static_cast<Hash>(h);
+	inline Hash HashBytes(void const* ptr, size_t len, const uint32_t seed = 19890212) noexcept {
+		Hash result;
+		if constexpr (sizeof(Hash) == 4)
+			Inner::MurmurHash3_x86_32(ptr, len, seed, &result);
+		else if constexpr (sizeof(Hash) == 8)
+			Inner::MurmurHash2_x64_64(ptr, len, seed, &result);
+		else if constexpr (sizeof(Hash) == 16)
+			Inner::MurmurHash3_x64_128(ptr, len, seed, &result);
+		else
+			TODO();
+		return result;
 	}
 
 	/// Hash a number																				
@@ -105,14 +491,18 @@ namespace Langulus
 	///	@param n - the number to hash														
 	///	@return the hash for the number													
 	template<CT::Number N>
-	constexpr Hash HashNumber(const N& n) noexcept {
-		auto x = static_cast<uint64_t>(n);
-		x ^= x >> 33U;
-		x *= UINT64_C(0xff51afd7ed558ccd);
-		x ^= x >> 33U;
-		//x *= UINT64_C(0xc4ceb9fe1a85ec53);
-		//x ^= x >> 33U;
-		return static_cast<Hash>(x);
+	constexpr Hash HashNumber(const N& n, const uint32_t seed = 19890212) noexcept {
+		constexpr bool tail = 0 != (sizeof(N) % sizeof(Hash));
+		Hash result;
+		if constexpr (sizeof(Hash) == 4)
+			Inner::MurmurHash3_x86_32<tail>(&n, sizeof(N), seed, &result);
+		else if constexpr (sizeof(Hash) == 8)
+			Inner::MurmurHash2_x64_64<tail>(&n, sizeof(N), seed, &result);
+		else if constexpr (sizeof(Hash) == 16)
+			Inner::MurmurHash3_x64_128<tail>(&n, sizeof(N), seed, &result);
+		else
+			TODO();
+		return result;
 	}
 
 	/// Hash any hashable data, including fundamental types							
@@ -146,7 +536,7 @@ namespace Langulus
 		}
 		else if constexpr (sizeof...(T) > 1) {
 			// Combine all data into a single array of hashes, and then		
-			// hash that																	
+			// hash that array again													
 			const Hash coalesced[sizeof...(T)] {HashData(data)...};
 			return HashBytes(coalesced, sizeof(Hash) * sizeof...(T));
 		}
@@ -154,3 +544,7 @@ namespace Langulus
 	}
 
 } // namespace Langulus
+
+// Let's not pollute the namespace
+#undef BIG_CONSTANT
+
